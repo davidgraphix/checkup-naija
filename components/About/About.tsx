776d6e5ea9b79856541b02ef6843dd7fe30332aa
David smart <112566573+davidgraphix@users.noreply.github.com>
2025-09-19 12:44:10 +0100

    Setup development environment

---
+++ components/About/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div><h1>About us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde saepe, vel nisi dolor eius et odit esse cumque deleniti in! Nulla possimus pariatur quidem suscipit at, aliquam sunt nisi ratione?</p></div>
+  )
+}
+
+export default About
