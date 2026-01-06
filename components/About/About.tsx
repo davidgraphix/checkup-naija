292ba378fc035d59a365b6a4b3f1acc190215ecb
David smart <112566573+davidgraphix@users.noreply.github.com>
2026-01-06 08:12:39 +0100

    Imported Icons

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Globe, Eye, Heart, Lightbulb, Hand as Hands, CheckCircle } from "lucide-react";
 import Navbar from "../Navbar/Navbar";
+import { Footer } from "../Footer/Footer";
+
 
 const About = () => {
   return (
@@ -8,7 +11,12 @@ const About = () => {
                 bg-[length:250%_250%] animate-[diagonal-flow_10s_linear_infinite]"
     >
       <Navbar />
+       {/* Hero Section with Who We Are */}
     
+
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 };
 
